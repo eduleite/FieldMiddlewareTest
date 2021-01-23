ffcc8c00bfbb8a970ac8254bd2a70541f1f2a563
Eduardo Bastos Leite <eduardo.leite@petrobras.com.br>
2021-01-23 18:24:58 -0300

    First commit

---
+++ FieldMiddlewareTest/QueryResolver.cs
@@ -0,0 +1,20 @@
+﻿using HotChocolate.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FieldMiddlewareTest
+{
+
+    public class QueryResolver
+    {
+
+        [UseUpperCase]
+        public string HelloWorld()
+        {
+            return "hello world";
+        }
+
+    }
+}
